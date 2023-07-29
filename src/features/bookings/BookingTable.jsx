725477c84725c886acee97790a08dd29b34e8e0b
Josphat Muteru <58342288+josphatmuteru@users.noreply.github.com>
2023-07-29 22:04:16 +0300

    API side filtering and sorting bookings, building a reusable pagination component, API side pagination and prefetching

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -5,12 +5,13 @@ import Empty from "../../ui/Empty";
 
 import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
+import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
-  const { bookings, isLoading } = useBookings();
+  const { bookings, isLoading, count } = useBookings();
   if (isLoading) return <Spinner />;
 
-  if (!bookings.length) return <Empty resourceName="Bookings" />;
+  if (!bookings) return <Empty resourceName="Bookings" />;
 
   return (
     <Menus>
@@ -30,6 +31,9 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+        <Table.Footer>
+          <Pagination count={count} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
